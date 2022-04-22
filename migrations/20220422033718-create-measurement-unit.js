5df87bde01f9bbd87208755ea8f07185f1edea39
fiosman <fares.osman@outlook.com>
2022-04-21 23:39:44 -0400

    Update migration

--- migrations/20220422033718-create-measurement-unit.js
+++ migrations/20220422033718-create-measurement-unit.js
@@ -1,27 +1,29 @@
-'use strict';
+"use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('MeasurementUnits', {
+    return queryInterface.createTable("MeasurementUnits", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
-        type: Sequelize.STRING
+        allowNull: false,
+        type: Sequelize.STRING(20),
+        unique: true,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('MeasurementUnits');
-  }
+    return queryInterface.dropTable("MeasurementUnits");
+  },
 };
