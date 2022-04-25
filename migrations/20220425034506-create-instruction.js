d114aeaf01ad3f9b7731dc1447a4e7c68c5b5f59
fiosman <fares.osman@outlook.com>
2022-04-24 23:46:58 -0400

    update instruction migration

--- migrations/20220425034506-create-instruction.js
+++ migrations/20220425034506-create-instruction.js
@@ -1,33 +1,37 @@
-'use strict';
+"use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Instructions', {
+    return queryInterface.createTable("Instructions", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       specification: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
+        allowNull: false,
       },
       listOrder: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
       },
       recipeId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { Model: "Recipes" },
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Instructions');
-  }
+    return queryInterface.dropTable("Instructions");
+  },
 };
