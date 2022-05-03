ec9b99aa6ae0d9f7f0f1024aacf662748034dba4
fiosman <fares.osman@outlook.com>
2022-05-02 23:38:31 -0400

    Adds association for ingredient and recipes

--- models/ingredient.js
+++ models/ingredient.js
@@ -1,12 +1,17 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Ingredient = sequelize.define('Ingredient', {
-    amount: DataTypes.DECIMAL,
-    measurementUnitId: DataTypes.INTEGER,
-    foodStuff: DataTypes.STRING,
-    recipeId: DataTypes.INTEGER
-  }, {});
-  Ingredient.associate = function(models) {
+  const Ingredient = sequelize.define(
+    "Ingredient",
+    {
+      amount: DataTypes.DECIMAL,
+      measurementUnitId: DataTypes.INTEGER,
+      foodStuff: DataTypes.STRING,
+      recipeId: DataTypes.INTEGER,
+    },
+    {}
+  );
+  Ingredient.associate = function (models) {
+    Ingredient.hasMany(models.Recipe, { foreignKey: "recipeId" });
     // associations can be defined here
   };
   return Ingredient;
--- models/recipe.js
+++ models/recipe.js
@@ -9,6 +9,7 @@ module.exports = (sequelize, DataTypes) => {
   );
   Recipe.associate = function (models) {
     Recipe.hasMany(models.Instruction, { foreignKey: "recipeId" });
+    Recipe.belongsTo(models.Ingredient, { foreignKey: "recipeId" });
   };
   return Recipe;
 };
