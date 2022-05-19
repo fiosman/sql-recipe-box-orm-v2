18f0ae75aaca0ef65a4923bcfbc1da47ded9ba6d
fiosman <fares.osman@outlook.com>
2022-05-18 21:43:58 -0400

    adds logic to create new instruction

--- data-access-layer/instructions-repository.js
+++ data-access-layer/instructions-repository.js
@@ -1,24 +1,22 @@
-const { Op } = require('sequelize');
+const { Op } = require("sequelize");
 let Instruction;
 let moduleError;
 
 try {
-  const db = require('../models');
+  const db = require("../models");
   ({ Instruction } = db);
   if (Instruction === undefined) {
-    moduleError = 'It looks like you need to generate the Instruction model.';
+    moduleError = "It looks like you need to generate the Instruction model.";
   }
 } catch (e) {
   console.error(e);
-  if (e.message.includes('Cannot find module')) {
-    moduleError = 'It looks like you need initialize your project.';
+  if (e.message.includes("Cannot find module")) {
+    moduleError = "It looks like you need initialize your project.";
   } else {
     moduleError = `An error was raised "${e.message}". Check the console for details.`;
   }
 }
 /* Don't change code above this line ******************************************/
-
-
 
 async function createNewInstruction(specification, recipeId) {
   // Use the findAll method of the Instruction object to find all the
@@ -28,9 +26,10 @@ async function createNewInstruction(specification, recipeId) {
   // return it using the maximum listOrder from the query just before this.
   //
   // Docs: https://sequelize.org/v5/manual/instances.html#creating-persistent-instances
+  const newInstruction = await Instruction.create({ specification, recipeId });
+
+  return newInstruction;
 }
-
-
 
 /* Don't change code below this line ******************************************/
 module.exports = {
